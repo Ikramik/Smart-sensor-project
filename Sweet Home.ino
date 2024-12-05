3ebe6826d65cd419638d0d83a4e0232e22bcb592
Ikramik <96791130+Ikramik@users.noreply.github.com>
2024-12-05 16:01:17 +0000

    Save uncommitted changes

--- Sweet Home.ino
+++ Sweet Home.ino
@@ -1,49 +1,30 @@
 #include <DHT.h>
-
 #include <LiquidCrystal_I2C.h>
-
 #include <Wire.h>
-
-
 
 LiquidCrystal_I2C lcd(0x3F,16,2);   
 
 #define DHTPIN 3     // what pin we're connected to
-
 #define DHTTYPE DHT11  
-
 DHT dht(DHTPIN, DHTTYPE); 
 
 int Input = A0;
-
 int Buzzer = 2;
-
 int value;
-
 int MAX = 600;
-
 int h;  //Stores humidity value
-
 int t; //Stores temperature value
-
 
 
 void setup(){
 	Serial.begin(9600);
-
 	Serial.println("Air Test");
-
 	dht.begin();
-
 	lcd.init(); //initialize the lcd
-
 	lcd.backlight(); //open the backlight
-
 	pinMode(Input ,INPUT);    
-	
 	pinMode(Buzzer ,OUTPUT);
 }
-
 
 void loop(){
 	value = analogRead(A0);
@@ -63,11 +44,9 @@ void loop(){
     //Read data and store it to variables h (humidity) and t (temperature)
 
 	h = dht.readHumidity();
-
 	t = dht.readTemperature();
 
     //Print temp and humidity values to serial monitor
-
 	if (isnan(h) || isnan(t)) {
 		Serial.println("Failed to read from DHT sensor!");
 		lcd.setCursor(0, 1);
